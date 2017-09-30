74f76126a7cb33b75e6b1529bb9376a06046efdd
Mantas_MSI <mantasling@gmail.com>
2017-10-01 00:07:21 +0200

    Half of the core is done

---
+++ Assets/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	private GameObject readyButton;
+	private ShapesMove[] shapesMove;
+
+	void Start () {
+		shapesMove = GameObject.FindObjectsOfType<ShapesMove> ();
+		readyButton = GameObject.Find ("ReadyButton");
+	}
+
+	public void RoundStart(){
+		//Makes all the shapes move when the button is pressed
+		foreach (ShapesMove geoShape in shapesMove) {
+			geoShape.ShapesStartMovement();
+		}
+		//Disable ready button
+		readyButton.SetActive(false);
+	}
+}
---
+++ Assets/ShapesMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapesMove : MonoBehaviour {
+
+	private Rigidbody2D rb;
+
+	public float shapeSpeed = 2.5f;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	public void  ShapesStartMovement(){
+		//Regulates the direction
+		if (this.tag == "Player1Shapes") {
+			Movement (1f);
+		} else if(this.tag == "Player2Shapes")  {
+			Movement (-1);
+		}
+	}
+
+	void Movement(float direction){
+		//Movement itself
+		Vector2 moveDirection = new Vector2 (direction, 0f);
+		rb.velocity = moveDirection * shapeSpeed;
+	}
+}
